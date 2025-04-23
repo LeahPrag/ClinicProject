625dbd24926d532525871d87aa98415564937fd5
LeahPrag <Hand3D28@mbjcomp.org.il>
2025-04-23 18:05:35 +0300

    apointmentManager

--- fullstackProject/DAL/service/ApointmentManager.cs
+++ fullstackProject/DAL/service/ApointmentManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DAL.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,26 @@ namespace DAL.service
 {
     internal class ApointmentManager
     {
+        public DB_Manager db;
         public Boolean MakeAnApointment(string doctor_firtsname, string doctor_lastname, string client_firstname, string client_lastname)
         {
+            List<Client> clients = db.Clients.ToList();
+            int c=clients.FirstOrDefault( x => x.FirstName.Equals(client_firstname) && x.LastName.Equals(client_lastname)).ClientId;
+            List<Doctor> doctors = db.Doctors.ToList();
+            int d = doctors.FirstOrDefault(x => x.FirstName.Equals(doctor_firtsname) && x.LastName.Equals(doctor_lastname)).DoctorId;
+            if (d==null || c==null )
+            {
+                return false;
+            }
 
-            return;
+            return true;
+        }
+        public DateTime CheckFirstApoitmentAvailable()
+        {
+            foreach (Doctor d in db.Doctors)
+            {
+
+            }
         }
     }
 }
