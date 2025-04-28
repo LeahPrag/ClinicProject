08f4b7c4231d8eaab5fb5db8b974062483d29445
LeahPrag <Hand3D28@mbjcomp.org.il>
2025-04-28 15:46:26 +0300

    Added new models: AvailableQueue, Day, and DayDoctor

---
+++ fullstackProject/DAL/Models/Day.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Models;
+
+public partial class Day
+{
+    public int Id { get; set; }
+
+    public int DayNum { get; set; }
+
+    public int StartHour { get; set; }
+
+    public int EndHour { get; set; }
+
+    public virtual ICollection<DayDoctor> DayDoctors { get; set; } = new List<DayDoctor>();
+}
