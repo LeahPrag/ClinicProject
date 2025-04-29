c916d933bf28e07bb2074f94640fef71c467f9d3
LeahPrag <Hand3D28@mbjcomp.org.il>
2025-04-29 18:02:56 +0300

    add Clients query

--- fullstackProject/DAL/Models/Client.cs
+++ fullstackProject/DAL/Models/Client.cs
@@ -14,6 +14,7 @@ public partial class Client
     public string? Phone { get; set; }
 
     public string? Email { get; set; }
+    public string IDNumber { get; set; } = null!;
 
     public DateOnly BirthDate { get; set; }
 
