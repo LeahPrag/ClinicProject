4e778f23210f6ff28e5480647d365fe7c373a45e
HANAmelloul8394 <h0583218394@gmail.com>
2025-05-19 00:01:38 +0300

    relativePath to data base

--- fullstackProject/DAL/Models/DB_Manager.cs
+++ fullstackProject/DAL/Models/DB_Manager.cs
@@ -28,9 +28,20 @@ public partial class DB_Manager : DbContext
     public virtual DbSet<Doctor> Doctors { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=H:\\לאהלה\\project correct\\ClinicProject\\fullstackProject\\DAL\\data\\db.mdf;Integrated Security=True");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string relativePath = @"Data\DB.mdf"; // או ClinicDB.mdf, תלוי בשם האמיתי
+            string fullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath);
 
+            string connectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;
+                                     AttachDbFilename={fullPath};
+                                     Integrated Security=True;
+                                     Connect Timeout=30";
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AvailableQueue>(entity =>
